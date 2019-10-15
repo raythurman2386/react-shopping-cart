40c2b3e17e06204c093f7570f62b01e11e1f32c8
raythurman2386 <raymondthurman5@gmail.com>
2019-10-15 11:31:58 -0400

    imported useContext

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react'
 
 // Components
-import Product from './Product';
+import Product from './Product'
 
 const Products = props => {
-	return (
-		<div className="products-container">
-			{props.products.map(product => (
-				<Product
-					key={product.id}
-					product={product}
-					addItem={props.addItem}
-				/>
-			))}
-		</div>
-	);
-};
+  return (
+    <div className='products-container'>
+      {props.products.map(product => (
+        <Product key={product.id} product={product} addItem={props.addItem} />
+      ))}
+    </div>
+  )
+}
 
-export default Products;
+export default Products
