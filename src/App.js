c3540e856aa5a67fd98c92f8181ebe2ba176267f
raythurman2386 <raymondthurman5@gmail.com>
2019-10-15 22:31:35 -0400

    minor refactor complete

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import data from './data'
 import Navigation from './components/Navigation'
 import Products from './components/Products'
 import ShoppingCart from './components/ShoppingCart'
-import { ProductContext } from './contexts/ProductContext'
+import { ProductContext } from './contexts'
 import { CartContext } from './contexts'
 
 function App() {
