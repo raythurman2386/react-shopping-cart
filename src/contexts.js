1babb8f46e2a64f7dc09228f4e57373ca9ea9041
raythurman2386 <raymondthurman5@gmail.com>
2019-10-16 19:34:19 -0400

    destructured my providers

--- src/contexts.js
+++ src/contexts.js
@@ -2,3 +2,6 @@ import { createContext } from 'react'
 
 export const CartContext = createContext()
 export const ProductContext = createContext()
+
+export const CartProvider = CartContext.Provider
+export const ProductProvider = ProductContext.Provider
