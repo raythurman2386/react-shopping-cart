c9363799927c3517af9f2e6f1174f685273d2765
raythurman2386 <raymondthurman5@gmail.com>
2019-10-15 22:54:20 -0400

    initial creation of the localstorage hook

---
+++ src/useLocalStorage.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+
+const useLocalStorage = initialValue => {
+  const [storedCart, setStoredCart] = useState(() => {
+    const item = window.localStorage.getItem('Cart')
+    return item ? JSON.parse(item) : initialValue
+  })
+
+  const setCart = updatedValue => {
+    setStoredCart(updatedValue)
+
+    window.localStorage.setItem('Cart', JSON.stringify(updatedValue))
+  }
+
+  const addItem = item => {
+    setCart([...storedCart, item])
+  }
+
+  const removeItem = (e, id) => {
+    e.preventDefault()
+    const newCart = storedCart.filter(item => item.id !== id)
+
+    setCart(newCart)
+  }
+
+  return [storedCart, setCart, addItem, removeItem]
+}
